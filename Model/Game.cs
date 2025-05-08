cf17e22adc74fbd0aef6f5c0a0c17cd96d154881
JuanMa Garcia <juanmgar@gmail.com>
2025-05-08 19:16:47 +0200

    Añadimos el campo descripción

--- Model/Game.cs
+++ Model/Game.cs
@@ -6,5 +6,6 @@
         public string Title { get; set; }
         public string Platform { get; set; }
         public string Genre { get; set; }
+        public string? Description { get; set; }
     }
 }
